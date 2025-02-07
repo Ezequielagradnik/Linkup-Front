82f5cd3cb4c5c22c3c3558b0a155afe58ca50b3a
Ezequielagradnik <ezequielagradnik@gmail.com>
2025-02-07 15:08:22 -0300

    .

---
+++ .next/static/chunks/app_forgot-password_page_jsx_72fdc4._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_forgot-password_page_jsx_72fdc4._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_forgot-password_page_jsx_72fdc4._.js",
+  "chunks": [
+    "static/chunks/_510c5e._.js"
+  ],
+  "source": "dynamic"
+});
