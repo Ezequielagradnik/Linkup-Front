db7176fdc23a69593a88dc71434ed60187de084d
Ezequielagradnik <ezequielagradnik@gmail.com>
2025-02-10 15:19:06 -0300

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NTsnNhVI6oKogLIrZoumqg5xDJJO903ItggJCh6wfL4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
