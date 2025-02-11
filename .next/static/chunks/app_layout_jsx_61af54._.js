387cfca92bd8674d95a97055f5200b9277dd15c2
Ezequielagradnik <ezequielagradnik@gmail.com>
2025-02-11 13:19:45 -0300

    .

---
+++ .next/static/chunks/app_layout_jsx_61af54._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_jsx_61af54._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_jsx_61af54._.js",
+  "chunks": [
+    "static/chunks/_fd74a8._.css",
+    "static/chunks/node_modules_26c7fc._.js",
+    "static/chunks/_fdf0c3._.js"
+  ],
+  "source": "dynamic"
+});
